1f580e4c069b2db07587cf185a6b3f77a3639b97
Jalil Nourmohammadi Khiarak <jalil.nourmohammadi89@gmail.com>
2025-10-31 14:13:02 +0100

    working on merge sort

--- Data Structures and Algorithms/sorting.py
+++ Data Structures and Algorithms/sorting.py
@@ -16,3 +16,38 @@ def insertion_sort(arr):
             else:
                 break
     return arr
+
+def swap(arr, left, right):
+    swap_var = arr[left]
+    arr[right] = swap_var
+    arr[left] = swap_var
+    return arr
+
+def merge(arr, left, mid, right):
+    i = left
+    j = mid + 1
+    temp_arr = []
+    while i <= mid and j <= right:
+        if arr[i] < arr[j]:
+            temp_arr.append(arr[i])
+            i = i + 1
+        else:
+            temp_arr.append(arr[j])
+            j = j + 1
+    if i < mid:
+        temp_arr.extend(arr[i:mid])
+    if j < right:
+        temp_arr.extend(arr[j:right])
+    return temp_arr
+
+def merge_sort(arr, left, right):
+    if left >= right:
+        return arr
+    if left + 1 == right:
+        if arr[left] > arr[right]:
+            return swap(arr, left, right)
+    else:
+        mid = (left + right) // 2
+        merge_sort(arr, left, mid)
+        merge_sort(arr, mid+1, right)
+        merge(arr, left, mid, right)     
